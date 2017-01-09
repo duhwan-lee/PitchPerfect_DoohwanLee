b4c030d33be7b42fd109ed0253af0e3ae5a6f47f
enghks1414 <doohwanlee@doohwans-Mac.local>
2017-01-09 21:33:08 +0900

    playsoundviewController-audio add

--- Pitch-Perfect/PlaySoundsViewController.swift
+++ Pitch-Perfect/PlaySoundsViewController.swift
@@ -7,12 +7,58 @@
 //
 
 import UIKit
+import AVFoundation
 
 class PlaySoundsViewController: UIViewController {
-var recordedAudioURL: URL!
+    var recordedAudioURL: URL!
+    
+    @IBOutlet weak var stopButton: UIButton!
+    @IBOutlet weak var reverbButton: UIButton!
+    @IBOutlet weak var echoButton: UIButton!
+    @IBOutlet weak var vaderButton: UIButton!
+    @IBOutlet weak var chipmunkButton: UIButton!
+    @IBOutlet weak var rabbitButton: UIButton!
+    @IBOutlet weak var snailButton: UIButton!
+
+    var audioFile:AVAudioFile!
+    var audioEngine:AVAudioEngine!
+    var audioPlayerNode: AVAudioPlayerNode!
+    var stopTimer: Timer!
+    
+    enum ButtonType: Int {
+        case slow = 0, fast, chipmunk, vader, echo, reverb
+    }
+    @IBAction func playSoundForButton(_ sender: UIButton) {
+    print("play sound button pressed")
+        switch (ButtonType(rawValue:sender.tag)!) {
+        case .slow:
+            playSound(rate: 0.5)
+        case .fast:
+            playSound(rate: 1.5)
+        case .chipmunk:
+            playSound(pitch: 1000)
+        case .vader:
+            playSound(pitch: -1000)
+        case .echo:
+            playSound(echo: true)
+        case .reverb:
+            playSound(reverb: true)
+        }
+        
+        configureUI(.playing)
+    }
+    @IBAction func stopButtonPressed(_ sender: Any) {
+    print("stop audio button pressed")
+    stopAudio()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+        configureUI(.notPlaying)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setupAudio()
         // Do any additional setup after loading the view.
     }
 
